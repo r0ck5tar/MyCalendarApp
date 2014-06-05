a3f6c9200e4c78865013693348065aabc341edc5
Hakim bin Luqman <hakim@hakimluqman.com>
2014-06-05 23:58:53 +0200

    Can now create Events
    Demo is now ready.

--- CalendarModule/src/main/java/fr/unice/polytech/calendarmodule/FreeTimeCalendarService.java
+++ CalendarModule/src/main/java/fr/unice/polytech/calendarmodule/FreeTimeCalendarService.java
@@ -10,6 +10,14 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
 import android.provider.CalendarContract;
+import android.provider.CalendarContract.*;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 /**
  * Created by Hakim on 5/6/2014.
@@ -67,21 +75,87 @@ public class FreeTimeCalendarService extends Service {
 
     public Uri createCalendar(){
         ContentValues values = new ContentValues();
-        values.put(CalendarContract.Calendars.ACCOUNT_NAME, "freetime");
-        values.put(CalendarContract.Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
-        values.put(CalendarContract.Calendars.NAME, "FreeTime Calendar");
-        values.put(CalendarContract.Calendars.CALENDAR_DISPLAY_NAME, "FreeTime Calendar");
-        values.put( CalendarContract.Calendars.CALENDAR_COLOR, 0xffff0000);
-        values.put(CalendarContract.Calendars.CALENDAR_ACCESS_LEVEL, CalendarContract.Calendars.CAL_ACCESS_OWNER);
-        values.put(CalendarContract.Calendars.OWNER_ACCOUNT, "freetime@gmail.com");
-        values.put(CalendarContract.Calendars.CALENDAR_TIME_ZONE, "Europe/Paris");
-        Uri.Builder builder = CalendarContract.Calendars.CONTENT_URI.buildUpon();
-        builder.appendQueryParameter(CalendarContract.Calendars.ACCOUNT_NAME, "com.freetime");
-        builder.appendQueryParameter(CalendarContract.Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
+        values.put(Calendars.ACCOUNT_NAME, "freetime");
+        values.put(Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
+        values.put(Calendars.NAME, "FreeTime Calendar");
+        values.put(Calendars.CALENDAR_DISPLAY_NAME, "FreeTime Calendar");
+        values.put(Calendars.CALENDAR_COLOR, 0xffff0000);
+        values.put(Calendars.CALENDAR_ACCESS_LEVEL, Calendars.CAL_ACCESS_OWNER);
+        values.put(Calendars.OWNER_ACCOUNT, "freetime@gmail.com");
+        values.put(Calendars.CALENDAR_TIME_ZONE, "Europe/Paris");
+        Uri.Builder builder = Calendars.CONTENT_URI.buildUpon();
+        builder.appendQueryParameter(Calendars.ACCOUNT_NAME, "com.freetime");
+        builder.appendQueryParameter(Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
         builder.appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER,"true");
         Uri uri = getContentResolver().insert(builder.build(), values);
 
         System.out.println(uri);
+        Toast.makeText(getApplicationContext(), "Created FreeTime Calendar" , Toast.LENGTH_LONG).show();
         return uri;
+    }
+
+    public long getFreeTimeCalendarId() {
+        String[] projection = new String[]{Calendars._ID};
+        String selection = Calendars.ACCOUNT_NAME + " = ? AND "
+                         + Calendars.ACCOUNT_TYPE +  " = ? ";
+        // use the same values as above:
+        String[] selArgs =
+                new String[]{"freetime", CalendarContract.ACCOUNT_TYPE_LOCAL};
+        Cursor cursor = getContentResolver().query(Calendars.CONTENT_URI,
+                projection, selection, selArgs, null);
+        if (cursor.moveToFirst()) {
+            return cursor.getLong(0);
+        }
+        else{
+            Uri uri = createCalendar();
+            cursor = getContentResolver().query(uri, projection, null, null, null);
+            if (cursor.moveToFirst()) {
+                return cursor.getLong(0);
+            }
+            return 0;
+        }
+    }
+
+    public long createEvent(String title, int startDay, int startMonth, int startYear,
+                            int endDay, int endMonth, int endYear) {
+        long calId = getFreeTimeCalendarId();
+
+        Calendar cal = new GregorianCalendar(startYear, startMonth, startDay);
+        cal.setTimeZone(TimeZone.getTimeZone("UTC"));
+        cal.set(Calendar.HOUR, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        long start = cal.getTimeInMillis();
+
+        cal = new GregorianCalendar(endYear, endMonth, endDay);
+        cal.setTimeZone(TimeZone.getTimeZone("UTC"));
+        cal.set(Calendar.HOUR, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        long end = cal.getTimeInMillis();
+
+        ContentValues values = new ContentValues();
+        values.put(Events.DTSTART, start);
+        values.put(Events.DTEND, end);
+        //values.put(Events.RRULE, "FREQ=DAILY;COUNT=20;BYDAY=MO,TU,WE,TH,FR;WKST=MO");
+        values.put(Events.TITLE, title);
+        values.put(Events.EVENT_LOCATION, "Münster");
+        values.put(Events.CALENDAR_ID, calId);
+        values.put(Events.EVENT_TIMEZONE, "Europe/Paris");
+        values.put(Events.DESCRIPTION, "The agenda or some description of the event");
+        // reasonable defaults exist:
+        values.put(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);
+        values.put(Events.SELF_ATTENDEE_STATUS, Events.STATUS_CONFIRMED);
+        values.put(Events.ALL_DAY, 1);
+        values.put(Events.ORGANIZER, "demo@freetime.com");
+        values.put(Events.GUESTS_CAN_INVITE_OTHERS, 1);
+        values.put(Events.GUESTS_CAN_MODIFY, 1);
+        values.put(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
+        Uri uri = getContentResolver().insert(Events.CONTENT_URI, values);
+        long eventId = new Long(uri.getLastPathSegment());
+
+        return eventId;
     }
 }
--- app/src/main/java/fr/unice/polytech/mycalendarapp/app/MainActivity.java
+++ app/src/main/java/fr/unice/polytech/mycalendarapp/app/MainActivity.java
@@ -4,32 +4,40 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Date;
 
 import fr.unice.polytech.calendarmodule.FreeTimeCalendarService;
 
 public class MainActivity extends ActionBarActivity {
     private FreeTimeCalendarService ftcService;
+    private DatePicker startDatePicker;
+    private DatePicker endDatePicker;
+    private EditText eventTitle;
 
     private ServiceConnection ftcServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder service) {
             ftcService = ((FreeTimeCalendarService.FreeTimeBinder)service).getService();
 
-            Toast.makeText(getApplicationContext(), "FreeTimeCalendarService connected", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "@string/service_connected", Toast.LENGTH_LONG).show();
         }
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
             ftcService = null;
 
-            Toast.makeText(getApplicationContext(), "FreeTimeCalendarService disconnected", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "@string/service_disconnected", Toast.LENGTH_LONG).show();
         }
     };
 
@@ -45,8 +53,31 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View view) {
                 System.out.println("Reading Calendar");
-                ftcService.createCalendar();
-                ftcService.getAllCalendars();
+                System.out.println(ftcService.getFreeTimeCalendarId());
+            }
+        });
+
+        eventTitle = (EditText) findViewById(R.id.eventTitle);
+        startDatePicker = (DatePicker) findViewById(R.id.startdatePicker);
+        endDatePicker = (DatePicker) findViewById(R.id.endDatePicker);
+
+        Button createEventButton = (Button) findViewById(R.id.createEventButton);
+        createEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Creating Event");
+                String title = eventTitle.getText().toString();
+                int startDay = startDatePicker.getDayOfMonth();
+                int startMonth = startDatePicker.getMonth();
+                int startYear = startDatePicker.getYear();
+                int endDay = endDatePicker.getDayOfMonth();
+                int endMonth = endDatePicker.getMonth();
+                int endYear = endDatePicker.getYear();
+
+                System.out.println("Start date :" + startDay + "/"  + startMonth + "/" + startYear);
+                System.out.println("End date :" + endDay + "/" + endMonth + "/" + endYear);
+
+                ftcService.createEvent(title, startDay, startMonth, startYear, endDay, endMonth, endYear);
             }
         });
 
